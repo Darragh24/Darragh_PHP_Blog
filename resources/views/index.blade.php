835a20621a8e6529249c5b9b38b2915af35823e2
Darragh24 <conwaydarragh03@gmail.com>
2024-03-10 22:07:56 +0000

    Changed background image on index page using custom css in resources/css/app.css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <div class="flex text-gray-100 pt-10">
         <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block text-center">
             <h1 class="sm:text-white text-5xl uppercase font-bold text-shadow-md pb-14">
-                Hello
+                Would you like to become a developer?
             </h1>
             <a href="/blog" class="text-center bg-gray-50 text-gray-700 py-2 px-4 font-bold text-xl uppercase">
                 Read More
